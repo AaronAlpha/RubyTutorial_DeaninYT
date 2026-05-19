1383ee99641f195002f26a61fff45545a394869c
AaronAlpha <reachaaronbernard@gmail.com>
2026-05-19 15:57:37 -0600

    Initial commit

---
+++ Episode 1/main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# the above statement is required to ensure that string (str) literals are set to be immutable
+# because: "Everything in Ruby is an Object", thus a str, meant to be a literal can have its state change, if not including above comment
+
+# 19.0.26
+
+puts 'Hello World!' # convention for printing is to use '' (single-quote)
